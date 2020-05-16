fafc0394747904e3fdf1fa1ecd0ca8713261ef0c
LMGV <gorkunvoevoda@mail.ru>
2020-05-16 14:45:18 +0200

    cleaning

---
+++ scripts/cleaning.R
@@ -0,0 +1,26 @@
+library(readxl)
+library(xts)
+library(quantmod)
+
+setwd("C:/Users/user/iCloudDrive/SG MiQEF/Financial Volatility/Project/GitHub/data")
+
+oil <- read_excel("Oil.xlsx")
+rub <- read_excel("RUBUSD.xlsx")
+
+oil <- xts(oil$DCOILBRENTEU, order.by = oil$DATE)
+oil<-oil[!(oil[,1] =="."),] #NA?s are coded as "."
+
+rub <- xts(rub$`RUB/USD`, order.by = rub$data)
+
+ts <- merge(rub,oil,join='left')
+ts <- na.omit(ts)
+
+ts_r <- ts
+ts_r$rub <- dailyReturn(ts$rub, type='log')
+ts_r$oil <- dailyReturn(ts$oil, type='log')
+
+# Stationarity
+plot(ts_r$rub)
+plot(ts_r$oil)
+
+write.table(coredata(ts_r), "data.csv", row.names = index(ts_r), col.names = names(ts_r), sep=",")
