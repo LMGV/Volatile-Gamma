b3996e6086fd8c89c9d4453f936bf257cb779080
Dr.Watson <erik.senn@gmx.de>
2020-05-16 15:37:04 +0200

    add cumret plot

--- scripts/cleaning.R
+++ scripts/cleaning.R
@@ -1,27 +1,38 @@
-library(readxl)
-library(xts)
-library(quantmod)
+# Data Cleaning
 
-setwd("C:/Users/user/iCloudDrive/SG MiQEF/Financial Volatility/Project/GitHub/data")
+# librarys
+  library(readxl)
+  library(xts)
+  library(quantmod)
 
-oil <- read_excel("Oil.xlsx")
-rub <- read_excel("RUBUSD.xlsx")
+  setwd("C:/Users/user/iCloudDrive/SG MiQEF/Financial Volatility/Project/GitHub/data")
 
-oil <- xts(oil$DCOILBRENTEU, order.by = oil$DATE)
-oil<-oil[!(oil[,1] =="."),] #NA?s are coded as "."
+# Import Data ----
+  oil <- read_excel("Oil.xlsx")
+  rub <- read_excel("RUBUSD.xlsx")
 
-rub <- xts(rub$`RUB/USD`, order.by = rub$data)
+# Clean Data ----
+  oil <- xts(oil$DCOILBRENTEU, order.by = oil$DATE)
+  oil<-oil[!(oil[,1] =="."),] #NA?s are coded as "."
+  
+  rub <- xts(rub$`RUB/USD`, order.by = rub$data)
+  
+  ts <- merge(rub,oil,join='left')
+  ts <- na.omit(ts)
+  
+  ts_r <- ts
+  ts_r$rub <- dailyReturn(ts$rub, type='log')
+  ts_r$oil <- dailyReturn(ts$oil, type='log')
+  ts_r <- ts_r[-1, ]
 
-ts <- merge(rub,oil,join='left')
-ts <- na.omit(ts)
+# Short Descriptives: Stationarity? 
+  # plot cum logreturns
+  plot(exp(cumsum(ts_r$oil)), col = 'darkgreen', main="Oil (green) and Rub/USD (blue)")
+  lines(exp(cumsum(ts_r$rub)), col = 'blue',  type = 'l')
+  
+  # plot logreturns
+  plot(ts_r$rub)
+  plot(ts_r$oil)
 
-ts_r <- ts
-ts_r$rub <- dailyReturn(ts$rub, type='log')
-ts_r$oil <- dailyReturn(ts$oil, type='log')
-ts_r <- ts_r[-1, ]
-
-# Stationarity
-plot(ts_r$rub)
-plot(ts_r$oil)
-
-write.table(coredata(ts_r), "data.csv", row.names = index(ts_r), col.names = names(ts_r), sep=",")
+#save cleaned data
+  write.table(coredata(ts_r), "data.csv", row.names = index(ts_r), col.names = names(ts_r), sep=",")
