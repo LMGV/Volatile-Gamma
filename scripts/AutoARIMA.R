5dbfebd366f8b91cb8e36417dacb6b9171559626
LMGV <gorkunvoevoda@mail.ru>
2020-05-17 15:24:49 +0200

    choosing arima model. Finished?

--- scripts/AutoARIMA.R
+++ scripts/AutoARIMA.R
@@ -16,6 +16,7 @@ acf(data2$rub, lag.max = 30, plot = TRUE)
 
 # H0: independence
 Box.test(data$oil, lag = 2, type = "Ljung-Box")#$p.value
+Box.test(data$rub, lag = 2, type = "Ljung-Box")#$p.value
 #auto.arima(data$oil, stationary = TRUE, ic = "bic")
 #auto.arima(data$rub, stationary = TRUE, ic = "bic")
 
@@ -26,11 +27,16 @@ Box.test(data$oil, lag = 2, type = "Ljung-Box")#$p.value
 
 oil.t   <- data$oil[-1]
 oil.t_1 <- data$oil[-length(data$oil)]
-x       <- matrix(c(rep(1, length(oil.t_1)), oil.t_1), nrow = length(oil.t_1), ncol = 2) # matrix of a constant and lagged dep var
+x.oil   <- matrix(c(rep(1, length(oil.t_1)), oil.t_1), nrow = length(oil.t_1), ncol = 2) # matrix of a constant and lagged dep var
+
+rub.t   <- data$rub[-1]
+rub.t_1 <- data$rub[-length(data$rub)]
+x.rub   <- matrix(c(rep(1, length(rub.t_1)), rub.t_1), nrow = length(rub.t_1), ncol = 2) # matrix of a constant and lagged dep var
+
 
 ######### fit regression y_t = \alpha0 + \alpha1*y_{t-1} + \epsilon_t
 
-# this function performs a DF test with a constant and returns 1 if differencing is required and 0 otherwise
+# this function performs a DF test with a constant; returns 1 if differencing is required and 0 otherwise
 df.test <- function(y, X, cl){ # cl can be one of 0.01, 0.025, 0.05, 0.1
   if( !(cl %in% c(0.01, 0.025, 0.05, 0.1)) ) stop('cl must be one of 0.01, 0.025, 0.05, 0.1')
   betas.hat   <- solve(crossprod(X)) %*% t(X) %*% y
@@ -50,18 +56,8 @@ df.test <- function(y, X, cl){ # cl can be one of 0.01, 0.025, 0.05, 0.1
   return(d)
 }
 
-
-
-
-
-#crit.values <- c(-3.434, -3.120, -2.863, -2.568) # Dickey-Fuller critical values for 1%, 2.5%, 5% and 10% respectively
-
-#if (d.f.stat < crit.values[3]){                  # choose 5% significance level
-#  d <- 0
-#} else {
-#  d <- 1
-#}
-
+d.oil <- df.test(oil.t, x.oil, 0.05)
+d.rub <- df.test(rub.t, x.rub, 0.05)
 
 
 #### ARIMA model fit ####
@@ -72,7 +68,7 @@ arma.errors <- function(parameters, y, p, q){ # parameters should be passed as a
   epsilon    <- rep(0, length(y))
   epsilon[1] <- y[1] - parameters[1]
   
-  # all below - to allow for varying p and q
+  # to allow for varying p and q
   
   ar.term <- function(parameters, y, p, o){
     if (p == 0){return(0)}
@@ -168,16 +164,6 @@ arma.errors <- function(parameters, y, p, q){ # parameters should be passed as a
     }
   }
   
-  #for (i in ((p+1):length(y))){
-  #  epsilon[i] <- y[i] - parameters[1] - ar.term(parameters, y, p, i) - parameters[3]*epsilon[i-1]
-  #}
-  
-  
-  
-  #for (i in (2:length(y))){
-  #  epsilon[i] <- y[i] - parameters[1] - parameters[2]*y[i-1] - parameters[3]*epsilon[i-1]
-  #}
-  
   return(sum(epsilon^2))
   
 }
@@ -205,6 +191,7 @@ fit.ARIMA <- function(y, p, d, q){ # data vector, AR order, differencing, MA ord
   
 }
 
+# this function returns the best ARIMA model according to the BIC criterion
 best.ARIMA <- function(y, p_grid, d, q_grid){
   model.bic    <- 0
   
@@ -228,61 +215,6 @@ best.ARIMA <- function(y, p_grid, d, q_grid){
   return(best.model)
 }
 
-
-# 
-# regression: y_t = c + \epsilon_t + \phi_1*y_{t-1} + \theta_1\eta_{t-1}
-#arma.errors <- function(parameters, y){ # parameters should be passed as a vector of c, AR, MA
-#  epsilon <- rep(0, length(y))
-#  epsilon[1] <- y[1] - parameters[1]
-  
-#  for (i in (2:length(y))){
-#    epsilon[i] <- y[i] - parameters[1] - parameters[2]*y[i-1] - parameters[3]*epsilon[i-1]
-#  }
-  
-#  return(sum(epsilon^2))
-  
-#}
-
-#### some something (delete later) ####
-#arma.errors <- function(parameters){ # parameters should be passed as a vector of c, AR, MA
-#  epsilon <- rep(0, length(oil.t))
-#  epsilon[1] <- oil.t[1] - parameters[1]
-#  
-#  for (i in (2:length(oil.t))){
-#    epsilon[i] <- oil.t[i] - parameters[1] - parameters[2]*oil.t[i-1] - parameters[3]*epsilon[i-1]
-#  }
-#  
-#  return(sum(epsilon^2))
-#  
-#}
-
-#arma.coeffs <- nlm(f = arma.errors, p = c(0, 0, 0), y = oil.t)$gradient
-#arma.coeffs <- nlminb(objective = arma.errors, start = c(0, 0, 0))$par
-#arma.coeffs <- optim(par = c(0,0,0), fn = arma.errors, y = oil.t)$par
-
-#epsilon_mle <- rep(0, length(oil.t))
-#epsilon_mle[1] <- oil.t[1] - arma.coeffs[1]
-#for (i in (2:length(oil.t))){
-#  epsilon_mle[i] <- oil.t[i] - arma.coeffs[1] - arma.coeffs[2]*oil.t[i-1] - arma.coeffs[3]*epsilon_mle[i-1]
-#}
-
-#log(sum(epsilon_mle^2))
-#### ####
-
-# fit a log-likelihood function
-#log.lik <- function(parameters, y, p, q) {
-#  e  <- arma.errors(parameters, y, p, q)
-#  ll <- -(length(y)/2)*log(2*pi) - (length(y)/2)*log(parameters[length(parameters)]) - (1/(2*parameters[length(parameters)]))*e
-#  return(-1 * ll)
-#}
-#library(optimx)
-
-#arma.coeffs  <- optim(par = c(rep(0, p+q+1), 1), fn = log.lik, y = oil.t, p = p, q = q)$par # minimize the negative log-likelihood
-#loglik.value <- -1*log.lik(arma.coeffs, oil.t, p, q)                       # get the log-likelihood value
-#p = 1
-#q = 1
-#bic          <- -2*loglik.value + ((p + q + 1))*log(length(oil.t))   # compute the BIC
-
-
-
+model.oil <- best.ARIMA(oil.t, c(0, 1, 2), d.oil, c(0, 1, 2))
+model.rub <- best.ARIMA(rub.t, c(0, 1, 2), d.rub, c(0, 1, 2))
 
