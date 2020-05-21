ccf9c9243472294210e8eb16fda8c625ffb858dc
JCordier1 <65345256+JCordier1@users.noreply.github.com>
2020-05-21 14:22:36 +0200

    Update

--- scripts/dcc_call_function.R
+++ scripts/dcc_call_function.R
@@ -1,7 +1,8 @@
 source("C:/Users/johan/Documents/GitHub/Volatile-Gamma/scripts/dcc_v2.R")
 source("C:/Users/johan/Documents/GitHub/Volatile-Gamma/scripts/dcc_forecast.R")
 source("C:/Users/johan/Documents/GitHub/Volatile-Gamma/scripts/model_comp.R")
-
+library(zoo)
+library(xts)
 
 ####Import Univariate GARCH Models####
 full_sample <- readRDS("C:/Users/johan/Documents/GitHub/Volatile-Gamma/output/univariateModels/model_and_prediction.rds")
@@ -28,6 +29,7 @@ full_sample$dccestimates <- estimates
 
 
 ####TREE GARCH--------------------------------------------------------------------------
+####TREE Subsample 1####
 ####Import Univariate GARCH Models####
 rub_list_tree_1 <- full_sample[["models"]][["rub_tree"]][["rub_subsample1"]]
 oil_list_tree_1 <- full_sample[["models"]][["oil"]][["oil_all"]]
@@ -38,17 +40,58 @@ oil_pred_tree_1 <- full_sample[["predictions"]][["rub_tree_subsample1"]]
 dccoutput_tree_1 <- dcc_function(rub_list_tree_1,oil_list_tree_1,rub_pred_tree_1,oil_pred_tree_1)
 full_sample$dccoutput_tree_1 <- dccoutput_tree_1
 
-####DCC Estimation Standard GARCH####
+####DCC Estimation TREE-GARCH 1####
 pred_results_1_tree_1 <- full_sample[["predictions"]][["rub_tree_subsample1"]][["variance_predict"]]
-#pred_results_2_tree_1 <- full_sample[["predictions"]][["oil"]][["variance_predict"]] # i need them on the same date as rub
-dccfit_tree_1 <- full_sample[["dccoutput"]][["fit"]]
-returns_tree_1 <- full_sample[["dccoutput"]][["returns"]]
-covs_tree_1 <- full_sample[["dccoutput"]][["fit"]]@mfit[["Q"]]
+pred_results_2_tree_1 <- full_sample[["predictions"]][["oil"]]
+
+ts<- xts(full_sample[["predictions"]][["rub_tree_subsample1"]], order.by = full_sample[["predictions"]][["rub_tree_subsample1"]]$date)
+ts2 <- xts(full_sample[["predictions"]][["oil"]], order.by = full_sample[["predictions"]][["oil"]]$date)
+ 
+pred_results_2_tree_1 <- merge(ts$date,ts2$variance_proxy,join='left')
+pred_results_2_tree_1 <- as.numeric(pred_results_2_tree_1[,2])
+
+
+
+dccfit_tree_1 <- full_sample[["dccoutput_tree_1"]][["fit"]]
+returns_tree_1 <- full_sample[["dccoutput_tree_1"]][["returns"]]
+covs_tree_1 <- full_sample[["dccoutput_tree_1"]][["fit"]]@mfit[["Q"]]
 
 estimates_tree_1 <- dcc_forecast(dccfit_tree_1, pred_results_1_tree_1, pred_results_2_tree_1, returns_tree_1, covs_tree_1)
 full_sample$dccestimates_tree_1 <- estimates_tree_1
 
+####TREE Subsample 2####
+####Import Univariate GARCH Models####
+rub_list_tree_2 <- full_sample[["models"]][["rub_tree"]][["rub_subsample2"]]
+oil_list_tree_2 <- full_sample[["models"]][["oil"]][["oil_all"]]
+rub_pred_tree_2 <- full_sample[["predictions"]][["rub_tree_subsample2"]]
+oil_pred_tree_2 <- full_sample[["predictions"]][["rub_tree_subsample2"]]
 
+####Call DCC Funcitons####
+dccoutput_tree_2 <- dcc_function(rub_list_tree_2,oil_list_tree_2,rub_pred_tree_2,oil_pred_tree_2)
+full_sample$dccoutput_tree_2 <- dccoutput_tree_2
+
+
+
+rub_list <- rub_list_tree_2
+oil_list <- oil_list_tree_2
+rub_pred <- rub_pred_tree_2
+oil_pred <- oil_pred_tree_2
+
+####DCC Estimation TREE-GARCH 2####
+pred_results_1_tree_2 <- full_sample[["predictions"]][["rub_tree_subsample2"]][["variance_predict"]]
+pred_results_2_tree_2 <- full_sample[["predictions"]][["oil"]]
+
+ts <- xts(full_sample[["predictions"]][["rub_tree_subsample2"]], order.by = full_sample[["predictions"]][["rub_tree_subsample2"]]$date)
+ts2 <- xts(full_sample[["predictions"]][["oil"]], order.by = full_sample[["predictions"]][["oil"]]$date)
+pred_results_2_tree_2 <- merge(ts$date,ts2$variance_proxy,join='left')
+pred_results_2_tree_2 <- as.numeric(pred_results_2_tree_2[,2])
+
+dccfit_tree_2 <- full_sample[["dccoutput_tree_2"]][["fit"]]
+returns_tree_2 <- full_sample[["dccoutput_tree_2"]][["returns"]]
+covs_tree_2 <- full_sample[["dccoutput_tree_2"]][["fit"]]@mfit[["Q"]]
+
+estimates_tree_2 <- dcc_forecast(dccfit_tree_2, pred_results_1_tree_2, pred_results_2_tree_2, returns_tree_2, covs_tree_2)
+full_sample$dccestimates_tree_2 <- estimates_tree_2
 
 
 ####comparison####
